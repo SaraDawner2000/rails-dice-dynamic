80b8da883c0a6a6f9c5e8f1861a9bd0e17c84bc6
Natalie Demyanenko <83845798+SaraDawner2000@users.noreply.github.com>
2024-06-04 11:49:42 -0500

    build dice rolling functionality

--- app/controllers/dice_controller.rb
+++ app/controllers/dice_controller.rb
@@ -1,5 +1,8 @@
 class DiceController < ApplicationController
   def roll
+    @number_of_dice = params[:number_of_dice].to_i
+    @how_many_sides = params[:how_many_sides].to_i
+    @rolls = Array.new(@number_of_dice) {|roll| rand(1..@how_many_sides)}
     render({template:'throws/roll'})
   end
 end
